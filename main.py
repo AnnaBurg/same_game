75cd70a6e66c5cd6e717a3698df6973a45f2a60d
Roma <mail@mail.ru>
2023-04-19 17:14:29 +0400

    заливка, рисование, клик

---
+++ main.py
@@ -0,0 +1,50 @@
+from tkinter import *
+
+
+def draw():
+    canvas.delete("all")
+    w = 300 // m
+    h = 300 // n
+    for i in range(n):
+        for j in range(m):
+            canvas.create_rectangle(j * w, i * h, j * w + w, i * h + h, fill=colors[a[i][j]])
+
+
+def click(event):
+    column = event.x // 60
+    row = event.y // 60
+    print(column, row)
+    if a[row][column] != 0:
+        flood(a, row, column)
+        draw()
+
+
+def flood(a, row, column):
+    old_color = a[row][column]
+    a[row][column]=0
+    if a[row][column - 1] == old_color:
+        flood(a,row, column - 1)
+    if a[row - 1][column] == old_color:
+        flood(a, row - 1, column)
+    if a[row][column + 1] == old_color:
+        flood(a, row, column +1)
+    if a[row + 1][column] == old_color:
+        flood(a, row + 1, column)
+
+
+a = [[1, 1, 2, 2, 3, 0],
+     [2, 2, 1, 3, 1, 0],
+     [1, 3, 3, 2, 1, 0],
+     [2, 1, 1, 3, 3, 0],
+     [3, 1, 3, 2, 3, 0],
+     [0, 0, 0, 0, 0, 0]]
+
+colors = ["white", "red", "blue", "green"]
+m = n = 5
+window = Tk()
+window.title("same game")
+canvas = Canvas(window, height=300, width=300, bg="white")
+canvas.pack()
+draw()
+canvas.bind('<Button-1>', click)
+window.mainloop()
