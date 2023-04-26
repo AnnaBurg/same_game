865ed02dbb9c6545eb0980f8526336000313082e
Anna <mail@mail.ru>
2023-04-26 17:22:28 +0400

    fall

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from tkinter import *
 import random
 
 
-def draw(canvas, colors, a, n, m):
+def draw():
     canvas.delete("all")
     w = 300 // m
     h = 300 // n
@@ -18,6 +18,8 @@ def click(event):
     if a[row][column] != 0:
         flood(a, row, column)
         draw()
+        canvas.after(1000, fall)
+        canvas.after(1100, draw)
 
 
 def flood(a, row, column):
@@ -33,31 +35,35 @@ def flood(a, row, column):
         flood(a, row + 1, column)
 
 
-def fall(a):
-    pass
+def fall():
+    for i in range(10):
+        for i in range(5):
+            for j in range(5):
+                if a[i + 1][j] == 0:
+                    a[i + 1][j] = a[i][j]
+                    a[i][j] = 0
 
 
-def main():
-    a = [[1, 1, 2, 2, 3, 9],
-         [2, 2, 1, 3, 1, 9],
-         [1, 3, 3, 2, 1, 9],
-         [2, 1, 1, 3, 3, 9],
-         [3, 1, 3, 2, 3, 9],
-         [9, 9, 9, 9, 9, 9]]
+a = [[1, 1, 2, 2, 3, 9],
+     [2, 2, 1, 3, 1, 9],
+     [1, 3, 3, 2, 1, 9],
+     [2, 1, 1, 3, 3, 9],
+     [3, 1, 3, 2, 3, 9],
+     [9, 9, 9, 9, 9, 9]]
 
-    for i in range(5):
-        for j in range(5):
-            a[i][j] = random.randint(1, 3)
+for i in range(5):
+    for j in range(5):
+        a[i][j] = random.randint(1, 3)
 
-    colors = ["white", "red", "blue", "green"]
-    m = n = 5
-    window = Tk()
-    window.title("same game")
-    canvas = Canvas(window, height=300, width=300, bg="white")
-    canvas.pack()
-    draw(canvas, colors, a, n, m)
-    canvas.bind('<Button-1>', click)
-    window.mainloop()
+colors = ["white", "red", "blue", "green"]
+m = n = 5
+window = Tk()
+window.title("same game")
+canvas = Canvas(window, height=300, width=300, bg="white")
+canvas.pack()
+draw()
+canvas.bind('<Button-1>', click)
+window.mainloop()
 
 
-main()
+
